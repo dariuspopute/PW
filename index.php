420378c64f7d08552952643e2a68ce3f956dbb17
dariuspopute <poputedarius@gmail.com>
2017-10-04 22:11:17 +0300

    v2

---
+++ index.php
@@ -0,0 +1,37 @@
+<?php 
+
+?>
+
+<html>
+<head>
+	<title>War Cry Online</title>
+	<link rel="stylesheet" href="style/css/bootstrap.min.css" integrity="sha384-rwoIResjU2yc3z8GV/NPeZWAv56rSmLldC3R/AZzGRnGxQQKnKkoFVhFQhNUwEyJ" crossorigin="anonymous">
+	<script src="style/js/bootstrap.min.js" integrity="sha384-vBWWzlZJ8ea9aCX4pEW3rVHjgjt7zpkNpZk+02D9phzyeVkE+jo0ieGizqPLForn" crossorigin="anonymous"></script>
+	 <link rel="stylesheet" type="text/css" href="style_index.css">
+</head>
+
+<body>
+
+	<div class="jumbotron">
+	
+		<header>
+			<a class="margin-right" href="login.php">Log in</a>
+			<a class="margin-right" href="createaccount.php">Create acount</a>
+			<a class="margin-right" href="support.php">Contact support</a>
+		</header>
+	
+	
+		<div class = "container">
+			
+			<ul>
+				<p><a href="factions.php">Factions</a></p>
+				<p><a href="races.php">Races</a></p>
+				<p><a href="classes.php">Classes</a></p>
+			
+			</ul>
+		
+		</div>
+	
+	</div>
+</body>
+</html>
